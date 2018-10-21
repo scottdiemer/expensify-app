0a6e4bf69b7b5df80ac44624f9a4f89c8ba9940e
scottdiemer <sdiemer@cfl.rr.com>
2018-10-20 21:34:47 -0400

    worked on descructuring arrays

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+// Object destructuring
+//
+
 // const person = {
 //   name: 'Scott',
 //   age: 41,
@@ -16,14 +20,30 @@
 //   console.log(`It's ${temperature} in ${city}`);
 // }
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    name: 'Penguin'
-  }
-};
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// };
+//
+// const { name: publisherName = 'Self-Published'} = book.publisher;
+//   
+// console.log(publisherName); // Penguin, Self-Published
 
-const { name: publisherName = 'Self-Published'} = book.publisher;
-  
-console.log(publisherName); // Penguin, Self-Published
+//
+// Array destructuring
+//
+
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+//
+// const [, city, state = 'New York'] = address;
+//
+// console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [itemName,,mediumPrice] = item;
+
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
