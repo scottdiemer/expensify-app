f6e50fc741a716852eeb2c5c976d701fda5a9359
scottdiemer <sdiemer@cfl.rr.com>
2019-01-31 22:28:24 -0500

    setup react-redux provider and used connect on ExpenseList

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import getVisibleExpenses from './selectors/expenses'
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters'
-import 'normalize.css/normalize.css'
+import { setTextFilter } from './actions/filters';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
@@ -15,8 +16,18 @@ store.dispatch(addExpense({ description: 'Water bill' }));
 store.dispatch(addExpense({ description: 'Gas bill' }));
 store.dispatch(setTextFilter('water'));
 
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'));
+}, 3000)
+
 const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'));
